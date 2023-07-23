3e54288b532143a16ead1bb8d638270684938212
SexyBoy12 <russianthief3@gmail.com>
2023-07-23 14:52:14 +0300

    Initial commit

---
+++ src/test/java/com/example/springdatahltv/SpringDataBasicDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdatahltv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataBasicDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
